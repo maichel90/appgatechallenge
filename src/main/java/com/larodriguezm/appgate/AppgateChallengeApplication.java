c28f7eebc658307a3bcb5177195ad6d6646a5f85
larodriguezm <lamaichel@stefanini.com>
2021-06-19 10:49:55 -0500

    first commit

---
+++ src/main/java/com/larodriguezm/appgate/AppgateChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.larodriguezm.appgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppgateChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppgateChallengeApplication.class, args);
+	}
+
+}
